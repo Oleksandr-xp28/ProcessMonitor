c116cb55cee01616ca7e78214680c73da10e15e1
Oleksandr Fedorut <fedorut20@gmail.com>
2024-08-09 19:21:58 +0300

    error

--- ProcessMonitor/Model/ProcessInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ProcessMonitor.Model;
-public class ProcessInfo
-{
-	public uint Id { get; set; }
-	public string Name { get; set; } = string.Empty;
-	public string UserName { get; set; } = string.Empty;
-	public string Status { get; set; } = string.Empty;
-}
